1c80e24518c51de5102950b21627a8c36a12d62c
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-05-01 10:31:20 +0530

    added few more lines

---
+++ text.py
@@ -0,0 +1,7 @@
+# STUDENT ID CARD GENERATOR
+STUDENT_NAME=input("enter student name:")
+ROLL_NO=int(input("enter roll no:"))
+DEPARTMENT=input("enter your department:")
+YEAR=int(input("year of joining:"))
+
+
