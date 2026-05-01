078f1e12af96793f9ebbfbfad1961b14313430ef
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-05-01 10:36:50 +0530

    modified

--- text.py
+++ text.py
@@ -3,5 +3,4 @@ STUDENT_NAME=input("enter student name:")
 ROLL_NO=int(input("enter roll no:"))
 DEPARTMENT=input("enter your department:")
 YEAR=int(input("year of joining:"))
-
-
+print("f STUDENT ID : \n STUDENT NAME:{STUDENT_NAME} \n ROLL NO:{ROLL_NO} \n DEPARTMENT: {DEPARTMENT} \n YEAR{YEAR}") 
