8706d3617a402501cc8449f7b1de82b933c8af16
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-05-01 10:40:00 +0530

    deleted int

--- text.py
+++ text.py
@@ -1,6 +1,6 @@
 # STUDENT ID CARD GENERATOR
 STUDENT_NAME=input("enter student name:")
-ROLL_NO=int(input("enter roll no:"))
+ROLL_NO=input("enter roll no:")
 DEPARTMENT=input("enter your department:")
-YEAR=int(input("year of joining:"))
+YEAR=input("year of joining:")
 print("f STUDENT ID : \n STUDENT NAME:{STUDENT_NAME} \n ROLL NO:{ROLL_NO} \n DEPARTMENT: {DEPARTMENT} \n YEAR{YEAR}") 
